930163aac68cf74d2fe8c8798a55c1d665287c60
Ivan Franchin <ivan.franchin@takeaway.com>
2025-03-15 18:38:02 +0100

    Upgrade to Spring Boot 3.4.3

--- push-docker-images-spring-boot.sh
+++ push-docker-images-spring-boot.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 JAVA_VERSION=21
-SPRING_BOOT_VERSION=3.3.4
+SPRING_BOOT_VERSION=3.4.3
 
 docker push ivanfranchin/spring-boot-greetings-api-web:${SPRING_BOOT_VERSION}-${JAVA_VERSION}-jvm
 docker push ivanfranchin/spring-boot-greetings-api-web:${SPRING_BOOT_VERSION}-${JAVA_VERSION}-jvm-cds
